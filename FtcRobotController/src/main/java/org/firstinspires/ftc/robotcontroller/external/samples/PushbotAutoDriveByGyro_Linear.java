318be99ebc1321735afb178c08e0d00b67c403e3
Ftc Team 8045 <pleasanthillftc@gmail.com>
2018-10-31 21:42:38 -0700

    Add AutobyTime

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/PushbotAutoDriveByGyro_Linear.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/PushbotAutoDriveByGyro_Linear.java
@@ -72,6 +72,7 @@ import com.qualcomm.robotcore.util.Range;
 
 @Autonomous(name="Pushbot: Auto Drive By Gyro", group="Pushbot")
 @Disabled
+
 public class PushbotAutoDriveByGyro_Linear extends LinearOpMode {
 
     /* Declare OpMode members. */
