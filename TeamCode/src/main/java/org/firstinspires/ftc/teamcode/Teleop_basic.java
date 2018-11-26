7f0784b88ff0bd42f8dd4971cce20e55f0283d16
Ftc Team 8045 <pleasanthillftc@gmail.com>
2018-11-26 11:45:15 -0800

    Basic Teleop added from last year

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop_basic.java
@@ -0,0 +1,138 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import static java.lang.Math.abs;
+
+
+@TeleOp(name = "zMoo", group = "8045")  // @Autonomous(...) is the other common choice
+//@Disabled
+public class Teleop_basic extends OpMode {
+
+    Hardware8045 Cosmo;
+
+    // variables used during the configuration process
+
+
+    private ElapsedTime runtime = new ElapsedTime();
+    double timeLeft;
+
+//    double turnDirection;
+    public double speedMultiplier = 0.5 ;
+
+    //Booleans
+
+    public boolean aIsReleased = true;
+
+ 
+ 
+    @Override
+    public void init() {
+
+        Cosmo = new Hardware8045();
+        Cosmo.init(hardwareMap);
+        telemetry.addData("Status", "Initializing");
+        telemetry.update();
+
+        timeLeft = 120;
+
+        telemetry.addData("Finished", "Initializing");
+        telemetry.update();
+    }
+
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    @Override
+    public void loop() {
+        timeLeft = 120 - runtime.seconds();
+
+        /**
+         * DRIVE Functions HERE
+         */
+
+         drivesmart(-gamepad1.right_stick_x, -gamepad1.right_stick_y,  gamepad1.left_stick_x);
+
+
+        telemetry.addData("TimeLeft: ",timeLeft);
+        telemetry.addData("Right -X: ", -gamepad1.right_stick_x);
+        telemetry.addData("Right -Y: ", -gamepad1.right_stick_y);
+        telemetry.addData(" Left -X: ", -gamepad1.left_stick_x);
+        telemetry.update();
+
+    }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * This method puts the current thread to sleep for the given time in msec.
+     * It handles InterruptException where it recalculates the remaining time
+     * and calls sleep again repeatedly until the specified sleep time has past.
+     *
+     * @param sleepTime specifies sleep time in msec.
+     */
+    public static void sleep(long sleepTime) {
+        long wakeupTime = System.currentTimeMillis() + sleepTime;
+
+        while (sleepTime > 0) {
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+            }
+            sleepTime = wakeupTime - System.currentTimeMillis();
+        }
+    }   //sleep
+
+
+    public void drivesmart(double x, double y, double turn) {
+
+        double lfpower;
+        double lrpower;
+        double rfpower;
+        double rrpower;
+
+        double rotation = turn*0.75;  // knock down the turn power
+
+        //Determine largest power being applied in either direction
+
+        lfpower = ( y - x + rotation);
+        lrpower = ( y + x + rotation);
+        rfpower = ( y + x - rotation);
+        rrpower = ( y - x - rotation);
+
+//        //Determine largest power being applied in either direction
+        double max = abs(lfpower);
+        if (abs(lrpower) > max) max = abs(lrpower);
+        if (abs(rfpower) > max) max = abs(rfpower);
+        if (abs(rrpower) > max) max = abs(rrpower);
+
+//            double multiplier = speedMultiplier / max; //multiplier to adjust speeds of each wheel so you can have a max power of 1 on atleast 1 wheel
+        double multiplier = (speedMultiplier / max) + 0.2*Math.abs(x);  // try to boost up the strafing power
+
+        lfpower *= multiplier;
+        lrpower *= multiplier;
+        rfpower *= multiplier;
+        rrpower *= multiplier;
+
+        Cosmo.leftFront.setPower(lfpower);
+        Cosmo.leftRear.setPower(lrpower);
+        Cosmo.rightFront.setPower(rfpower);
+        Cosmo.rightRear.setPower(rrpower);
+    }
+//
+
+
+    public void stop() {
+        Cosmo.leftFront.setPower(0.0);
+        Cosmo.leftRear.setPower(0.0);
+        Cosmo.rightFront.setPower(0.0);
+        Cosmo.rightRear.setPower(0.0);
+    }
+
+
+
+
+}
