54dec9965b003d7104d57b391327eab168f5a53d
ftcteam8045 <pleasanthillftc@gmail.com>
2018-12-04 16:25:33 -0800

    testdrive needs the proper hardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_TestDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_TestDrive.java
@@ -35,7 +35,7 @@ import static org.firstinspires.ftc.teamcode.oldcode.DriveTrain.drive_THRESHOLD;
 public class Auto_TestDrive extends LinearOpMode {
 
     /* Declare OpMode members. */
-    Hardware8045testbot Cosmo = new Hardware8045testbot();   // Use a Pushbot's hardware
+    Hardware8045 Cosmo = new Hardware8045();   // Use a Pushbot's hardware
     private ElapsedTime runtime = new ElapsedTime();
     private ElapsedTime cycletime = new ElapsedTime();
     /**   Menu Parameter Initialization **/
